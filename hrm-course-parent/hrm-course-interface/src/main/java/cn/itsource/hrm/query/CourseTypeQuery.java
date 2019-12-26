1c8354fc19236382cf87515186c81cdbf44b3612
hejinze <1214069303@qq.com>
2019-12-26 17:09:43 +0800

    创建课程服务

---
+++ hrm-course-parent/hrm-course-interface/src/main/java/cn/itsource/hrm/query/CourseTypeQuery.java
@@ -0,0 +1,15 @@
+package cn.itsource.hrm.query;
+
+import cn.itsource.basic.query.BaseQuery;
+
+/**
+ * <p>
+ * 课程目录 查询参数对象
+ * </p>
+ *
+ * @author hjz
+ * @since 2019-12-26
+ */
+public class CourseTypeQuery extends BaseQuery {
+
+}
